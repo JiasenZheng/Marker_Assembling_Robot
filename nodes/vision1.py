6308c99a6e2cb6b9b82ccfe1e9a437361cd2b6b4
JiasenZheng <jiasenzheng2020@u.northwestern.edu>
2021-11-16 17:30:53 -0600

    modified vision1.py

--- nodes/vision1.py
+++ nodes/vision1.py
@@ -2,9 +2,7 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-if __name__ == "__main__":
-    file_name = "image.png"
+def detect_contour(file_name = "image.png"):
     img0 = cv.imread(file_name)
     img = cv.GaussianBlur(img0, (5, 5), 2)
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -29,8 +27,6 @@ if __name__ == "__main__":
             cs.append(c)
     
 
-
-    # plt.imshow(mask)
     figure = plt.figure()
     subplot1 = figure.add_subplot(1,2,1)
     subplot2 = figure.add_subplot(1,2,2)
@@ -38,3 +34,6 @@ if __name__ == "__main__":
     subplot1.imshow(cv.cvtColor(mask, cv.COLOR_BGR2RGB))
     subplot2.imshow(cv.cvtColor(img0, cv.COLOR_BGR2RGB))
     plt.show()
+
+if __name__ == "__main__":
+    detect_contour(file_name = "image.png")
