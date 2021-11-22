25043d4884b9274e35f2ae296710092db8ab087e
JiasenZheng <jiasenzheng2020@u.northwestern.edu>
2021-11-22 17:37:52 -0600

    modified the vision1 module

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
 from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
-d = generate_distutils_setup(
-    packages=['${py_pkg}'],
-    package_dir={'': 'src'}
-    )
-setup(**d)
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['vision'],
+    package_dir={'': 'src'})
+
+setup(**setup_args)
