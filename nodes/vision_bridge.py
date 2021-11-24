fa26d1bd0ad8dd7afca7019814fbac452d94f058
Bhagyesh Agresar <bhagyeshagresar@gmail.com>
2021-11-24 12:58:25 -0600

    added service and modified vision_bridge

--- nodes/vision_bridge.py
+++ nodes/vision_bridge.py
@@ -8,24 +8,49 @@ import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 import cv2
+import vision.vision1 as v1
+
 
 class Bridge:
 
     def __init__(self):
+        self.take_image = rospy.Service('ImageCapture', ImageCapture, self.capture_callback)
+        
         self.bridge = CvBridge()
-        self.image_publisher = rospy.Publisher("ros_image", Image, queue_size=10)
-        self.image_subscriber = rospy.Subscriber("usb_cam/image_raw", Image, self.callback)
+        self._pub = rospy.Publisher("image_processed", Image, queue_size = 10)
         self.rospy.init_node("vision_bridge")
 
     def callback(self,data):
 
-        #convert opencv image to ros image
+        # Output debugging information to the terminal
+        rospy.loginfo("receiving video frame")
+        
+        # Convert ROS Image message to OpenCV image
+        current_frame = self.bridge.imgmsg_to_cv2(data)
+        
+        # Display image
+        cv2.imshow("camera", current_frame)
+        
+        cv2.waitKey(1)
 
-        image_msg = self.bridge.cv2_to_imgmsg(data, "bgr8")
 
-        self.image_publisher(msg)
+        # manipulate with open cv
+        # low_res = cv2.blur(cv_image, (50, 50))
+        image_contour,grid = v1.detect_contour(current_frame, grid_size = (1,3), pixel_size = [475,125], starting_pixel = [125,200])
 
 
+        # convert to ROS
+        msg = self._bridge.cv2_to_imgmsg(image_contour, "bgr8")
+
+        self._pub.publish(msg)
+
+    def capture_callback(self, req):
+        image_subscriber = rospy.Subscriber("/camera/color/image_raw", Image, self.callback)
+
+
+        
 if __name__ == "__main__":
     b = Bridge()
     rospy.spin()
+
+
