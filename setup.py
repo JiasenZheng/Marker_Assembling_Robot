23375e1798f314e25b0f0ced9ce9be2b0f1d37f2
Kojo Welbeck <kojowelbeck2021@u.northwestern.edu>
2021-11-24 15:21:42 -0600

    Tweaks for demoing pick and place
    Adjusted fixed rpy; still tuning
    Adjusted z's for standoffs and pick and place
    Update setup to include manipulation python pkg

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['vision','xdot_k', 'match'],
+    packages=['vision','xdot_k', 'match', 'manipulation'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
