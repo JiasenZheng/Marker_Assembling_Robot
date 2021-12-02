54d533cb5529a2650dfa66c7900cb7697b30c09b
JiasenZheng <jiasenzheng2020@u.northwestern.edu>
2021-12-02 10:34:23 -0600

    modified setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['vision'],
+    packages=['vision1'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
