262c814060c5065f30a8795ca828368559b0592f
msr <iankennedy2022@u.northwestern.edu>
2021-12-03 16:24:58 -0600

    updating vision test package. still requires threshold values

--- test/test_vision.py
+++ test/test_vision.py
@@ -33,12 +33,40 @@ class VisionTestCase(unittest.TestCase):
                     "truth": (BABYBLUE, GREEN, PINK, YELLOW, GREEN, PINK, YELLOW, PURPLE, PURPLE)}
         self.A5 = { "img": "assembled5.png", 
                     "truth": (BABYBLUE, YELLOW, PINK, YELLOW, EMPTY, GREEN, PURPLE, YELLOW, ORANGE)}
-                                       
+
+
+        self.M1 = { "img": "markers2.png",
+                    "truth":  (BABYBLUE, ORANGE, GREEN, BABYBLUE,GREEN, PURPLE, PINK, YELLOW, YELLOW)}
+        
+        self.M2 = { "img": "markers3.png",
+                "truth":  (BABYBLUE, ORANGE, PURPLE, PURPLE, ORANGE, YELLOW, YELLOW, GREEN, GREEN)}
+
+        self.M3 = { "img": "markers4.png",
+                "truth":  (BABYBLUE, ORANGE, YELLOW, PURPLE, ORANGE, GREEN, PURPLE, YELLOW, GREEN)}
+
+        self.M4 = {"img": "markers5.png",
+                    "truth": (BABYBLUE, ORANGE, YELLOW, PINK, ORANGE, PURPLE, GREEN, YELLOW, PURPLE)}
+   
+
+
+        self.C1 = {"img": "caps1.png",
+                    "truth": (BABYBLUE, GREEN, BABYBLUE, ORANGE, YELLOW, PINK, YELLOW, GREEN, PURPLE)}
+
+        self.C2 = {"img": "caps2.png",
+                    "truth":  (YELLOW, PURPLE, PURPLE, PINK, GREEN, GREEN, PINK, BABYBLUE, ORANGE)}
+
+        self.C3 = {"img": "caps3.png",
+                    "truth": (YELLOW, PURPLE, PINK, PURPLE, PINK, GREEN, GREEN, BABYBLUE, ORANGE)}
+
+        self.C4 = {"img": "cap4.png",
+                    "truth": (YELLOW, PURPLE, PINK, PURPLE, ORANGE, YELLOW, GREEN, BABYBLUE, BABYBLUE)}
+                                                                                                
+    
     def create_test(self, test_dict):
 
         filepath = os.path.join(self.img_dir, test_dict["img"])
         img = cv.imread(filepath)
-        _, grid = detect_contour2(img)
+        _, grid = detect_contour2(img, (3,3), [640,480],[0,0])
         truth_arr = np.vstack(test_dict["truth"])
         print("grid: ", grid)
         print("truth_arr: ", grid)
@@ -50,6 +78,58 @@ class VisionTestCase(unittest.TestCase):
     def test_detectContours_A1(self):
         a1 = self.create_test(self.A1)
         self.assertTrue(a1)
+
+    def test_detectContours_A2(self):
+        a2 = self.create_test(self.A2)
+        self.assertTrue(a2)    
+
+    def test_detectContours_A3(self):
+        a3 = self.create_test(self.A3)
+        self.assertTrue(a3)
+
+    def test_detectContours_A4(self):
+        a4 = self.create_test(self.A4)
+        self.assertTrue(a4)
+
+    def test_detectContours_A5(self):
+        a5 = self.create_test(self.A5)
+        self.assertTrue(a5)
+
+
+
+    def test_detectContours_M1(self):
+        m1 = self.create_test(self.M1)
+        self.assertTrue(m1)
+
+    def test_detectContours_M2(self):
+        m2 = self.create_test(self.M2)
+        self.assertTrue(m2)
+
+    def test_detectContours_M3(self):
+        m3 = self.create_test(self.M3)
+        self.assertTrue(m3)
+
+    def test_detectContours_M4(self):
+        m4 = self.create_test(self.M4)
+        self.assertTrue(m4)
+
+
+
+    def test_detectContours_C1(self):
+        c1 = self.create_test(self.C1)
+        self.assertTrue(c1)
+
+    def test_detectContours_C2(self):
+        c2 = self.create_test(self.C2)
+        self.assertTrue(c2)
+
+    def test_detectContours_C3(self):
+        c3 = self.create_test(self.C3)
+        self.assertTrue(c3)
+
+    def test_detectContours_C4(self):
+        c4 = self.create_test(self.C4)
+        self.assertTrue(c4)     
 
 if __name__ == "__main__":
     import rosunit
