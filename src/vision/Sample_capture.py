c895fad0d31ef7c2449f9abe5f07b42f2cd6fe83
JiasenZheng <jiasenzheng2020@u.northwestern.edu>
2021-12-05 16:29:15 -0600

    Add docstings to Sample_capture script

--- src/vision/Sample_capture.py
+++ src/vision/Sample_capture.py
@@ -1,8 +1,11 @@
-# First import the library
+"""
+A helper python script to take and save sample pictures of markers that can be used to test on other
+detection algorithms
+"""
+
+
 import pyrealsense2 as rs
-# Import Numpy for easy array manipulation
 import numpy as np
-# Import OpenCV for easy image rendering
 import cv2
 
 # Create a pipeline
@@ -18,14 +21,6 @@ pipeline_profile = config.resolve(pipeline_wrapper)
 device = pipeline_profile.get_device()
 device_product_line = str(device.get_info(rs.camera_info.product_line))
 
-# found_rgb = False
-# for s in device.sensors:
-#     if s.get_info(rs.camera_info.name) == 'RGB Camera':
-#         found_rgb = True
-#         break
-# if not found_rgb:
-#     print("The demo requires Depth camera with Color sensor")
-#     exit(0)
 
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
@@ -33,15 +28,6 @@ config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 # Start streaming
 profile = pipeline.start(config)
 
-# # Getting the depth sensor's depth scale (see rs-align example for explanation)
-# depth_sensor = profile.get_device().first_depth_sensor()
-# depth_scale = depth_sensor.get_depth_scale()
-# print("Depth Scale is: " , depth_scale)
-
-# # We will be removing the background of objects more than
-# #  clipping_distance_in_meters meters away
-# clipping_distance_in_meters = 1 #1 meter
-# clipping_distance = clipping_distance_in_meters / depth_scale
 
 # Create an align object
 # rs.align allows us to perform alignment of depth frames to others frames
@@ -63,23 +49,9 @@ try:
         aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
         color_frame = aligned_frames.get_color_frame()
 
-        # Validate that both frames are valid
-        # if not aligned_depth_frame or not color_frame:
-        #     continue
 
         # depth_image = np.asanyarray(aligned_depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
-
-        # # Remove background - Set pixels further than clipping_distance to grey
-        # grey_color = 153
-        # depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
-        # bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
-
-        # # Render images:
-        # #   depth align to color on left
-        # #   depth on right
-        # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-        # images = np.hstack((bg_removed, depth_colormap))
 
         cv2.namedWindow('RGB Images', cv2.WINDOW_NORMAL)
         cv2.imshow('RGB Images', color_image)
@@ -88,6 +60,7 @@ try:
         if key & 0xFF == ord('q') or key == 27:
             cv2.destroyAllWindows()
             break
+        # Press 'a' to capture an image 
         elif key & 0xFF == ord('a'):
             cv2.imwrite('image1.png',color_image)
 finally:
