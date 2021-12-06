f3d4358e1c25d1247507695fddf4b34bb6a8a29c
JiasenZheng <jiasenzheng2020@u.northwestern.edu>
2021-12-05 18:36:01 -0600

    Adjust bounding boxes

--- src/vision/vision1.py
+++ src/vision/vision1.py
@@ -144,28 +144,13 @@ def plot_image(filename):
     plt.show()
 
 
-        
 
-    ## assembled
-    image1 = cv.imread('pictures/assembled.png')
-    img1, grid1 = detect_contour2(image1,(3,3), [450, 300], [110, 64])
-    print("assembled: ", grid1)
-    
-    ## caps1
-    image2 = cv.imread('pictures/caps1.png')
-    img2, grid2 = detect_contour2(image2,(3,3), [640, 480], [0, 0])
-    print("caps1: ", grid2)
-
-    ## markers2
-    image3 = cv.imread('pictures/markers2.png')
-    img3, grid3 = detect_contour2(image3,(3,3), [580, 450], [0, 0])
-    print("markers2: ", grid3)
 
 
 if __name__ == "__main__":
     # Test the detect_contour2 function with the sample images
-    image = cv.imread('pictures/assembled3.png')
-    image, list_H = detect_contour2(image,grid_size=(3,3),pixel_size = (550,390), starting_pixel = (380,130))
+    image = cv.imread('pictures/RepositionedPhotos/marker1.png')
+    image, list_H = detect_contour2(image,grid_size=(3,3),pixel_size=(750,530),starting_pixel=(200,100))
 
     print(list_H)
     plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB))
